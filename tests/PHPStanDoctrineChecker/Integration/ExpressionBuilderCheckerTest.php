1df27e9f84c87fd78221ed77271664b4166199b5
Stefan Siegl <stefan.siegl@mayflower.de>
2017-08-18 23:39:42 +0200

    replace ExprEqViolationTest by lighter (unit) test

--- tests/PHPStanDoctrineChecker/Integration/ExpressionBuilderCheckerTest.php
+++ tests/PHPStanDoctrineChecker/Integration/ExpressionBuilderCheckerTest.php
@@ -2,12 +2,42 @@
 
 namespace PHPStanDoctrineChecker\Integration;
 
+use Doctrine\ORM\QueryBuilder;
+use PHPStan\Analyser\Scope;
+use PHPStan\Analyser\TypeSpecifier;
+use PHPStan\Broker\Broker;
+use PHPStan\Type\ObjectType;
+use PHPStanDoctrineChecker\QueryBuilderInfo;
+use PHPStanDoctrineChecker\Service\QueryBuilderTracer;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\PrettyPrinter\Standard;
+
 class ExpressionBuilderCheckerTest extends IntegrationTestCase
 {
-    public function testExprEqViolation()
+    public function testExprEq()
     {
-        $errors = $this->runAnalyse(__DIR__ . '/data/ExprEqViolationTest.php');
-        $this->assertSingleError('DQL Query uses invalid filtered fetch-join', 13, $errors);
+        $queryBuilderInfo = new QueryBuilderInfo('u');
+
+        $this->runAndWhereWithExpressionBuilder($queryBuilderInfo, 'eq', [
+            new Arg(new String_('p.type')),
+            new Arg(new String_(':type')),
+        ]);
+
+        $this->assertEquals(['p'], $queryBuilderInfo->getDirtyAliases());
+    }
+
+    public function testExprEqWithoutFieldName()
+    {
+        $queryBuilderInfo = new QueryBuilderInfo('u');
+
+        $this->runAndWhereWithExpressionBuilder($queryBuilderInfo, 'eq', [
+            new Arg(new String_('x')),
+            new Arg(new String_(':phoneNumber')),
+        ]);
+
+        $this->assertEquals(['x'], $queryBuilderInfo->getDirtyAliases());
     }
 
     public function testExprLteViolation()
@@ -32,5 +62,52 @@ class ExpressionBuilderCheckerTest extends IntegrationTestCase
     {
         $errors = $this->runAnalyse(__DIR__ . '/data/ExprInViolationTest.php');
         $this->assertSingleError('DQL Query uses invalid filtered fetch-join', 13, $errors);
+    }
+
+    /**
+     * @param QueryBuilderInfo $queryBuilderInfo
+     * @param string $exprMethodName
+     * @param Arg[] $exprMethodArgs
+     */
+    protected function runAndWhereWithExpressionBuilder(
+        QueryBuilderInfo $queryBuilderInfo,
+        string $exprMethodName,
+        array $exprMethodArgs
+    )
+    {
+        $methodCall = new Expr\MethodCall(
+            new Expr\Variable('queryBuilder'),
+            'andWhere',
+            [
+                new Arg(
+                    new Expr\MethodCall(
+                        new Expr\MethodCall(
+                            new Expr\Variable('queryBuilder'),
+                            'expr'
+                        ),
+                        $exprMethodName,
+                        $exprMethodArgs
+                    )
+                ),
+            ]
+        );
+
+        $prettyPrinter = new Standard();
+        $scope = new Scope(
+            $this->getContainer()->getByType(Broker::class),
+            $prettyPrinter,
+            new TypeSpecifier($prettyPrinter),
+            'some_file_name',
+            null,
+            false,
+            null,
+            null,
+            null,
+            [
+                'queryBuilder' => new ObjectType(QueryBuilder::class),
+            ]
+        );
+
+        (new QueryBuilderTracer())->processNode($queryBuilderInfo, $methodCall, $scope);
     }
 }
